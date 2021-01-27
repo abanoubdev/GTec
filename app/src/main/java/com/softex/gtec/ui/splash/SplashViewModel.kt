1eccc4f6e1d1170d6f634d63750f506a1d01f151
Abanoub <abanoub.cww@gmail.com>
2021-01-28 00:23:37 +0200

    Salt Encryption algorithm is done

--- app/src/main/java/com/softex/gtec/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/softex/gtec/ui/splash/SplashViewModel.kt
@@ -1,6 +1,16 @@
 package com.softex.gtec.ui.splash
 
+import android.util.Log
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import com.softex.gtec.repository.RepositorySource
+import com.softex.gtec.util.SaltEncryption
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-class SplashViewModel : ViewModel() {
+@ExperimentalCoroutinesApi
+class SplashViewModel
+@ViewModelInject
+constructor(
+    private val mainRepository: RepositorySource
+) : ViewModel() {
 }
--- app/src/main/java/com/softex/gtec/util/SaltEncryption.kt
+++ app/src/main/java/com/softex/gtec/util/SaltEncryption.kt
@@ -5,11 +5,13 @@ import android.util.Base64
 object SaltEncryption {
 
     private const val firstSalt: String = "*&*$(#-j90"
-    private const val secondSalt: String = "*&*$(#-j90" + "(\$34#45(*)"
+    private const val secondSalt: String = "(\$34#45(*)"
 
     fun encrypt(txt: String): String {
-        val str = firstSalt + txt + secondSalt
-        return Base64.encode(str.toByteArray(Charsets.US_ASCII), Base64.DEFAULT)
-            .toString(charset("UTF-8"))
+        return Base64.encode(
+            "$firstSalt$txt$secondSalt".toByteArray(Charsets.US_ASCII),
+            Base64.DEFAULT
+        )
+            .toString(charset("UTF-8")).replace("\n", "")
     }
 }
