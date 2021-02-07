535a0ec016ea9c278bdb5deb4e1c9cc209958cb8
Abanoub <abanoub.cww@gmail.com>
2021-02-07 20:00:40 +0200

    Add empty forget password screen

---
+++ app/src/main/java/com/softex/gtec/ui/forgetPassword/ForgetPasswordFragment.kt
@@ -0,0 +1,22 @@
+package com.softex.gtec.ui.forgetPassword
+
+import androidx.fragment.app.viewModels
+import com.softex.gtec.R
+import com.softex.gtec.ui.BaseFragment
+import com.softex.gtec.ui.homepage.HomepageViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class ForgetPasswordFragment : BaseFragment(R.layout.forget_password_fragment) {
+
+    private val viewModel: HomepageViewModel by viewModels()
+    
+    override fun initViews() {
+    }
+
+    override fun subscribeObservers() {
+       
+    }
+}
---
+++ app/src/main/java/com/softex/gtec/ui/forgetPassword/ForgetPasswordViewModel.kt
@@ -0,0 +1,16 @@
+package com.softex.gtec.ui.forgetPassword
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.softex.gtec.repository.RepositorySource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@HiltViewModel
+class ForgetPasswordViewModel
+@ViewModelInject
+constructor(
+    private val mainRepository: RepositorySource
+) : ViewModel() {
+}
