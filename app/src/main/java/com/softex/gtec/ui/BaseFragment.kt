69c9c0e7a6b6f55248dd189841c2cd7d5d56a005
Abanoub <abanoub.cww@gmail.com>
2021-02-09 21:39:47 +0200

    remove unused import

--- app/src/main/java/com/softex/gtec/ui/BaseFragment.kt
+++ app/src/main/java/com/softex/gtec/ui/BaseFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.softex.gtec.ui.loading.LoadingDialog
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 abstract class BaseFragment(layoutId: Int) : Fragment(layoutId) {
 
