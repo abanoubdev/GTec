9d00ae5d924322b738d1f303e7754c549f0206e3
Abanoub <abanoub.cww@gmail.com>
2021-02-22 19:52:15 +0200

    Add equals and hashcode for all models

--- app/src/main/java/com/softex/gtec/model/featuredImages/BannerResponseItem.kt
+++ app/src/main/java/com/softex/gtec/model/featuredImages/BannerResponseItem.kt
@@ -8,4 +8,36 @@ data class BannerResponseItem(
     val HTML_RawText: String,
     val HTML_Text: String,
     val HTML_URL: String
-)
+){
+    override fun toString(): String {
+        return "{AltText='$AltText', FileGUID='$FileGUID', FileID=$FileID, FileName='$FileName', HTML_RawText='$HTML_RawText', HTML_Text='$HTML_Text', HTML_URL='$HTML_URL'}"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BannerResponseItem
+
+        if (AltText != other.AltText) return false
+        if (FileGUID != other.FileGUID) return false
+        if (FileID != other.FileID) return false
+        if (FileName != other.FileName) return false
+        if (HTML_RawText != other.HTML_RawText) return false
+        if (HTML_Text != other.HTML_Text) return false
+        if (HTML_URL != other.HTML_URL) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = AltText.hashCode()
+        result = 31 * result + FileGUID.hashCode()
+        result = 31 * result + FileID
+        result = 31 * result + FileName.hashCode()
+        result = 31 * result + HTML_RawText.hashCode()
+        result = 31 * result + HTML_Text.hashCode()
+        result = 31 * result + HTML_URL.hashCode()
+        return result
+    }
+}
--- app/src/main/java/com/softex/gtec/model/newArrivals/NewArrivalsResponseItem.kt
+++ app/src/main/java/com/softex/gtec/model/newArrivals/NewArrivalsResponseItem.kt
@@ -10,4 +10,42 @@ data class NewArrivalsResponseItem(
     val ItemPriceBeforeDiscount: Int,
     val ListFeaturedImages: List<FeaturedImages>,
     val ShortDescription: String
-)
+) {
+    override fun toString(): String {
+        return "{DiscountPercentage=$DiscountPercentage, ItemGroupID=$ItemGroupID, ItemGroupName='$ItemGroupName', ItemID=$ItemID, ItemName='$ItemName', ItemPrice=$ItemPrice, ItemPriceBeforeDiscount=$ItemPriceBeforeDiscount, ListFeaturedImages=$ListFeaturedImages, ShortDescription='$ShortDescription'}"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NewArrivalsResponseItem
+
+        if (DiscountPercentage != other.DiscountPercentage) return false
+        if (ItemGroupID != other.ItemGroupID) return false
+        if (ItemGroupName != other.ItemGroupName) return false
+        if (ItemID != other.ItemID) return false
+        if (ItemName != other.ItemName) return false
+        if (ItemPrice != other.ItemPrice) return false
+        if (ItemPriceBeforeDiscount != other.ItemPriceBeforeDiscount) return false
+        if (ListFeaturedImages != other.ListFeaturedImages) return false
+        if (ShortDescription != other.ShortDescription) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = DiscountPercentage
+        result = 31 * result + ItemGroupID
+        result = 31 * result + ItemGroupName.hashCode()
+        result = 31 * result + ItemID
+        result = 31 * result + ItemName.hashCode()
+        result = 31 * result + ItemPrice
+        result = 31 * result + ItemPriceBeforeDiscount
+        result = 31 * result + ListFeaturedImages.hashCode()
+        result = 31 * result + ShortDescription.hashCode()
+        return result
+    }
+
+
+}
